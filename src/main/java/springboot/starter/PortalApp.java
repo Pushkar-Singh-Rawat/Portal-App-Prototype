be3add320b7b56a70c22193fcd1ccb0e354271ac
desiCoda <j.pushkarsingh@gmail.com>
2018-02-18 22:09:50 +0530

    added springbootapplication class

---
+++ src/main/java/springboot/starter/PortalApp.java
@@ -0,0 +1,15 @@
+package springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PortalApp.class, args);
+		
+	}
+
+}
